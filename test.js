554e8e00b94ddcdfc177c319aee21fc5034f6774
Awal07 <awalamoo7@gmail.com>
2021-09-30 00:39:14 +0100

    New update

---
+++ test.js
@@ -0,0 +1,16 @@
+// fizzbuzz game
+
+
+function fizzbuzz (input) {
+    if (typeof input !== 'number') 
+    return NaN
+    else if ( (input%3 == 0) && (input%5 == 0) )
+    return "Fizzbuzz"
+    else if  (input%3 == 0)
+    return "Fizz"
+    else if (input%5 == 0)
+    return "Buzz"
+    else return input
+}
+const goal = fizzbuzz('e')
+console.log (goal);
