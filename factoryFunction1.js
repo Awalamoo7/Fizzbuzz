cb398ba45f8dd63bbe0819edd5aa723ad1b759bc
Awal07 <awalamoo7@gmail.com>
2021-10-04 01:35:04 +0100

    newfsctoryfuncttion for address

---
+++ factoryFunction1.js
@@ -0,0 +1,13 @@
+
+
+function showAddress (street, city, zipCode) {
+    return {
+        street,
+        city,
+        zipCode
+    }
+}
+
+let myAddress = showAddress ('ogundeji', 'ibadan', 2348)
+
+console.log (myAddress)
